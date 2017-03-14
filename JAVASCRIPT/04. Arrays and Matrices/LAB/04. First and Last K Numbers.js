2e87aa485ffa0680b56c10bfae3e3e7249b21442
Zhechev <mitko_jechev@mail.bg>
2017-03-15 00:02:26 +0200

    Delete 04. First and Last K Numbers.js

--- JAVASCRIPT/04. Arrays and Matrices/LAB/04. First and Last K Numbers.js
+++
@@ -1,9 +0,0 @@
-function test(arr) {
-let k = arr[0]
-    arr.shift();
-
-    let left = arr.slice(0,k);
-    let right = arr.slice(arr.length-k, arr.length)
-    console.log(left);
-    console.log(right);
-}
